55e97c550b356b06bcd3b6e315ca0a053a863e9e
Eduardo Aguiar <edu.pessoal@outlook.com>
2023-08-24 19:18:34 -0300

    language tool and login page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// Get Element URL
 const button = document.getElementById('spectate');
+
+let language = navigator.language || navigator.userLanguage;
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   let tablink = tabs[0].url;
@@ -9,43 +10,54 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 function setUrl(tablink) {
   let spelltableLobby = "https://spelltable.wizards.com/lobby";
   let spelltableGame = "https://spelltable.wizards.com/game";
+  let spelltableLogin = "https://myaccounts.wizards.com/login?redirectTo=https://spelltable.wizards.com/lobby?login=true"
 
   if (tablink.includes(spelltableGame) && !tablink.includes("spectate")) {
+    if (language.includes("pt")) {
+      button.innerHTML = "Assistir";
+    } else button.innerHTML = "Spectate";
     button.setAttribute('href', tablink + "/?spectate=true");
     
-    // Open new tab
     button.addEventListener('click', function() {
       chrome.tabs.create({url: button.getAttribute('href')});
 
-      // Close popup
       window.close();
     });
   } else if (!tablink.includes(spelltableLobby) && !tablink.includes(spelltableGame)){
     button.setAttribute('href', spelltableLobby);
-    button.innerHTML = "Let's go to SpellTable";
+    if (language.includes("pt")) {
+      button.innerHTML = "Vamos para o SpellTable";
+    } else button.innerHTML = "Let's go to SpellTable";
     
-    // Open new tab
     button.addEventListener('click', function() {
       chrome.tabs.create({url: button.getAttribute('href')});
 
-      // Close popup
       window.close();
     });
   } else if (tablink.includes("spectate")) {
     button.setAttribute('href', spelltableLobby);
-    button.innerHTML = "Return to Lobby";
+    if (language.includes("pt")) {
+      button.innerHTML = "Voltar para o Lobby";
+    } else button.innerHTML = "Return to Lobby";
 
-    // Open in same tab
     button.addEventListener('click', function() {
       chrome.tabs.update({url: button.getAttribute('href')});
 
-      // Close popup
       window.close();
     });
-  } else {
-    // disable button and change text
+  } else if (tablink == spelltableLogin) {
     button.setAttribute('href', '#');
-    button.innerHTML = "Select the game";
+    if (language.includes("pt")) {
+      button.innerHTML = "Realize o login";
+    } else button.innerHTML = "Login to SpellTable";
+
+    button.setAttribute('disabled', 'disabled');
+  } else {
+    button.setAttribute('href', '#');
+    if (language.includes("pt")) {
+      button.innerHTML = "Selecione o jogo";
+    } else button.innerHTML = "Select the game";
+
     button.setAttribute('disabled', 'disabled');
   }
 }
