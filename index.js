c16eed6bf6c50990e9d62c8702edb76d5bc88bd6
Eduardo Aguiar <edu.pessoal@outlook.com>
2023-08-24 14:38:10 -0300

    spelltable spectate google extension

---
+++ index.js
@@ -0,0 +1,50 @@
+// Get Element URL
+const button = document.getElementById('spectate');
+
+chrome.tabs.getSelected(null, function(tab) {
+  setUrl(tab.url);
+});
+
+function setUrl(tablink) {
+  let spelltableLobby = "https://spelltable.wizards.com/lobby";
+  let spelltableGame = "https://spelltable.wizards.com/game";
+
+  if (tablink.includes(spelltableGame) && !tablink.includes("spectate")) {
+    button.setAttribute('href', tablink + "/?spectate=true");
+    
+    // Open new tab
+    button.addEventListener('click', function() {
+      chrome.tabs.create({url: button.getAttribute('href')});
+
+      // Close popup
+      window.close();
+    });
+  } else if (!tablink.includes(spelltableLobby) && !tablink.includes(spelltableGame)){
+    button.setAttribute('href', spelltableLobby);
+    button.innerHTML = "Let's go to SpellTable";
+    
+    // Open new tab
+    button.addEventListener('click', function() {
+      chrome.tabs.create({url: button.getAttribute('href')});
+
+      // Close popup
+      window.close();
+    });
+  } else if (tablink.includes("spectate")) {
+    button.setAttribute('href', spelltableLobby);
+    button.innerHTML = "Return to Lobby";
+
+    // Open in same tab
+    button.addEventListener('click', function() {
+      chrome.tabs.update({url: button.getAttribute('href')});
+
+      // Close popup
+      window.close();
+    });
+  } else {
+    // disable button and change text
+    button.setAttribute('href', '#');
+    button.innerHTML = "Select the game";
+    button.setAttribute('disabled', 'disabled');
+  }
+}
